02e3d7b6993803b1c9711f7b83a1d602da117ccd
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-10-20 21:41:36 -0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ import { useAuth } from "./hooks/useAuth"
 export const App = () => {
   const { user } = useAuth()
   return (
-    <main className={styles.contentWrapper}>
+    <main
+      className={`${styles.contentWrapper} ${!!user && styles.contentSigned}`}
+    >
       <MessageList />
       {!!user ? <SendMessageForm /> : <LoginBox />}
     </main>
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import io from "socket.io-client"
 import { api } from "../../service/api"
 
 import styles from "./styles.module.scss"
@@ -15,9 +16,27 @@ type MessageType = {
     avatar_url: string
   }
 }
+const messageQueue: MessageType[] = []
+
+const socket = io("http://localhost:4000")
+
+socket.on("new_message", (newMessage) => {
+  messageQueue.push(newMessage)
+})
 
 export const MessageList = () => {
   const [messages, setMessages] = useState<MessageType[]>([])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messageQueue.length > 0) {
+        setMessages((prevState) =>
+          [messageQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        )
+        messageQueue.shift()
+      }
+    }, 3000)
+  }, [])
 
   useEffect(() => {
     api.get<MessageType[]>("last3").then((response) => {
